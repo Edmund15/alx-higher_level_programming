d999be54033f57975654acbc2853ead80aba701c
Edmund15 <annohboateng15@gmail.com>
2023-12-01 01:51:19 +0000

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 from sys import argv
 
-num_args = len(argv) - 1
-plural_suffix = 's' if num_args != 1 else ''
+if __name__ == "__main__":
+
+    num_args = len(argv) - 1
+    plural_suffix = 's' if num_args != 1 else ''
 
 print("{:d} argument{:s}{}".format(
     num_args, plural_suffix, ':' if num_args > 0 else '.'))
---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+import os
+
+folder_path = os.path.join(os.path.expanduser("~"), "Desktop", "hidden_4_F")
+
+sys.path.append(folder_path)
+
+import hidden_4
+
+if __name__ == "__main__":
+    names = dir(hidden_4)
+for name in names:
+    if name[:2] != "__":
+        print(name)
