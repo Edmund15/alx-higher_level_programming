00a01fb3a95c06267ac0cb41bc06d7509c7d459b
Edmund15 <annohboateng15@gmail.com>
2023-12-20 19:12:31 +0000

    advance

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,76 @@
+#include <Python.h>
+
+/**
+ * print_python_list - Prints basic information about Python lists
+ * @p: PyObject pointer
+ */
+void print_python_list(PyObject *p)
+{
+    Py_ssize_t size, i;
+    PyObject *item;
+
+    if (!PyList_Check(p))
+    {
+        printf("  [ERROR] Invalid List Object\n");
+        return;
+    }
+
+    size = PyList_Size(p);
+    printf("[*] Python list info\n");
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+    for (i = 0; i < size; i++)
+    {
+        item = PyList_GetItem(p, i);
+        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+    }
+}
+
+/**
+ * print_python_bytes - Prints basic information about Python bytes
+ * @p: PyObject pointer
+ */
+void print_python_bytes(PyObject *p)
+{
+    Py_ssize_t size, i;
+    char *str;
+
+    if (!PyBytes_Check(p))
+    {
+        printf("  [ERROR] Invalid Bytes Object\n");
+        return;
+    }
+
+    size = PyBytes_Size(p);
+    str = PyBytes_AsString(p);
+
+    printf("[.] bytes object info\n");
+    printf("  size: %ld\n", size);
+    printf("  trying string: %s\n", str);
+
+    if (size > 10)
+        size = 10;
+
+    printf("  first %ld bytes: ", size);
+    for (i = 0; i < size; i++)
+        printf("%02x ", (unsigned char)str[i]);
+    printf("\n");
+}
+
+/**
+ * print_python_float - Prints basic information about Python float
+ * @p: PyObject pointer
+ */
+void print_python_float(PyObject *p)
+{
+    if (!PyFloat_Check(p))
+    {
+        printf("  [ERROR] Invalid Float Object\n");
+        return;
+    }
+
+    printf("[.] float object info\n");
+    printf("  value: %f\n", ((PyFloatObject *)p)->ob_fval);
+}
+
---
+++ 0x05-python-exceptions/Python.h
@@ -0,0 +1,11 @@
+#ifndef _LISTS_H_
+#def _LISTS_H_
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+
+
+
+#endif /* _LISTS_H_*/
